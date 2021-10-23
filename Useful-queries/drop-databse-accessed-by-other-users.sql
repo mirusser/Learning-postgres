766564f59248051b7b7c6818dfac7e85623e276f
mirusser <mirusser@gmail.com>
2021-10-23 20:23:24 +0200

    Added useful links to query: drop-databse-accessed-by-other-users

--- Useful-queries/drop-databse-accessed-by-other-users.sql
+++ Useful-queries/drop-databse-accessed-by-other-users.sql
@@ -5,3 +5,8 @@ WHERE pg_stat_activity.datname = 'target_db'
   AND pid <> pg_backend_pid();
 
 DROP DATABASE target_db;
+
+
+-- useful links :
+--https://www.leeladharan.com/drop-a-postgresql-database-if-there-are-active-connections-to-it/
+--https://stackoverflow.com/questions/664091/drop-a-database-being-accessed-by-another-users
